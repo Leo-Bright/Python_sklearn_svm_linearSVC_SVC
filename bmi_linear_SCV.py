8261b4ff05aabf17e7d434653578c3fdd16a466b
Leo Bright <buxingzhexiong@sina.com>
2018-11-27 16:44:10 +0800

    数据标注代码增加部分提取功能

--- bmi_linear_SCV.py
+++ bmi_linear_SCV.py
@@ -2,14 +2,21 @@ from sklearn import model_selection as cross_validation, svm, metrics
 import matplotlib.pyplot as plt
 import pandas as pd
 
-tbl = pd.read_csv("./data/bmi.csv")
+tbl = pd.read_csv("labeled_data/LINE_v1.0_labeled_filter.embeddings", header=None, sep=' ', index_col=0)
+# print(tbl.dtypes)
+label = tbl[65]
+del tbl[65]
+# w = tbl["weight"] / 100
+# h = tbl["height"] / 200
+# dimensions_64 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+#                  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
+#                  44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64]
 
-label = tbl["label"]
-w = tbl["weight"] / 100 
-h = tbl["height"] / 200 
-wh = pd.concat([w, h], axis=1)
+# wh = pd.concat(dimensions_64, axis=1)
 
-data_train, data_test, label_train, label_test = cross_validation.train_test_split(wh,label)
+data_train, data_test, label_train, label_test = cross_validation.train_test_split(tbl, label)
+
+print(label_test)
 
 clf = svm.LinearSVC()
 clf.fit(data_train, label_train)
--- data_labeling.py
+++ data_labeling.py
@@ -1,12 +1,13 @@
 import json
+import random
 
 
-f_labeled = open(r'data/deepwalk_v1.0_labeled.embeddings', 'w+')
-f_embeddings = open(r'data/deepwalk_highway_allNodes_64d_v1.0.embeddings', 'r')
+f_labeled = open(r'data/node2vec_v1.0_labeled_filter.embeddings', 'w+')
+f_embeddings = open(r'data/node2vec_highway_64d_v1.0.embeddings', 'r')
 f_nodes_selected = open(r'data/nodes_crossing.json', 'r')
 
 
-def label_embeddings(selected, embeddings, output):
+def label_embeddings(selected, embeddings, output, fraction=10):
     node_crossing = json.loads(selected.readline())
     crossing_count = 0
     for line in embeddings.readlines():
@@ -20,11 +21,14 @@ def label_embeddings(selected, embeddings, output):
             output.write(line + ' ' + 'crossing' + '\n')
             crossing_count += 1
         else:
+            rd = random.randint(0, 99) + 1
+            if rd > filter:
+                continue
             print('this is not a crossing')
             output.write(line + ' ' + 'normal' + '\n')
     print(crossing_count)
 
-label_embeddings(f_nodes_selected, f_embeddings, f_labeled)
+label_embeddings(f_nodes_selected, f_embeddings, f_labeled, fraction=1)
 
 f_labeled.close()
 f_embeddings.close()
