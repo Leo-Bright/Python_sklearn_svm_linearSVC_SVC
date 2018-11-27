e752d850efcd59be40be97069ecfbf2a02923674
Leo Bright <buxingzhexiong@sina.com>
2018-11-27 16:59:31 +0800

    数据标注代码增加部分提取功能

--- data_labeling.py
+++ data_labeling.py
@@ -2,7 +2,7 @@ import json
 import random
 
 
-f_labeled = open(r'data/node2vec_v1.0_labeled_filter.embeddings', 'w+')
+f_labeled = open(r'labeled_data/node2vec_v1.0_labeled_filter_2.embeddings', 'w+')
 f_embeddings = open(r'data/node2vec_highway_64d_v1.0.embeddings', 'r')
 f_nodes_selected = open(r'data/nodes_crossing.json', 'r')
 
@@ -17,18 +17,16 @@ def label_embeddings(selected, embeddings, output, fraction=10):
         if len(node_vec) < 10:
             continue
         if osmid in node_crossing:
-            print('label a crossing node')
             output.write(line + ' ' + 'crossing' + '\n')
             crossing_count += 1
         else:
             rd = random.randint(0, 99) + 1
-            if rd > filter:
+            if rd > fraction:
                 continue
-            print('this is not a crossing')
             output.write(line + ' ' + 'normal' + '\n')
-    print(crossing_count)
+    print("crossing count: ", crossing_count)
 
-label_embeddings(f_nodes_selected, f_embeddings, f_labeled, fraction=1)
+label_embeddings(f_nodes_selected, f_embeddings, f_labeled, fraction=2)
 
 f_labeled.close()
 f_embeddings.close()
