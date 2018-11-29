09fc231d0a1b46ae136c5548c3d13685b304674d
Leo Bright <buxingzhexiong@sina.com>
2018-11-29 11:26:30 +0800

    增加交叉验证部分

--- bmi_linear_SCV.py
+++ bmi_linear_SCV.py
@@ -2,7 +2,7 @@ from sklearn import model_selection as cross_validation, svm, metrics
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data_matrix = pd.read_csv("labeled_data/deepwalk_v1.0_labeled.embeddings", header=None, sep=' ', index_col=0)
+data_matrix = pd.read_csv("labeled_data/node2vec_v1.0_labeled_filter.embeddings", header=None, sep=' ', index_col=0)
 # print(tbl.dtypes)
 label = data_matrix[65]
 del data_matrix[65]
@@ -22,7 +22,6 @@ print(cl_report)
 
 # ===================================直接调用交叉验证评估模型==========================
 clf2 = svm.LinearSVC()
-scores = cross_validation.cross_val_score(clf2, data_matrix, label, cv=10, n_jobs=5) #cv为迭代次数。
-cross_validation.cross_validate()
+scores = cross_validation.cross_val_score(clf2, data_matrix, label, cv=4, n_jobs=4) #cv为迭代次数。n_jobs为并发线程数
 print(scores) # 打印输出每次迭代的度量值（准确度）
 print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)) # 获取置信区间。（也就是均值和方差）
