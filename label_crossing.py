38c5c927c3819215800246b071d18fcfd5c9664a
Leo <xinming.lai@qq.com>
2018-12-01 11:33:05 +0800

    调整代码和目录结构

--- label_crossing.py
+++ label_crossing.py
@@ -7,7 +7,7 @@ f_embeddings = open(r'porto/embedding/deepwalk/deepwalk_highway_64d.embeddings.t
 f_nodes_selected = open(r'porto/node/nodes_crossing.json', 'r')
 
 
-def label_embeddings(selected, embeddings, output, positive_sample=10000, other_sample=10000, fraction=1):
+def label_embeddings(selected, embeddings, output, fraction=1):
     node_crossing = json.loads(selected.readline())
     crossing_count = 0
     normal_count = 0
@@ -18,22 +18,19 @@ def label_embeddings(selected, embeddings, output, positive_sample=10000, other_
         if len(node_vec) < 10:
             continue
         if osmid in node_crossing:
-            if crossing_count < positive_sample:
-                output.write(line + ' ' + 'crossing' + '\n')
-                crossing_count += 1
+            output.write(line + ' ' + 'crossing' + '\n')
+            crossing_count += 1
         else:
-            if normal_count < other_sample:
-                rd = random.randint(0, 99) + 1
-                if rd > fraction:
-                    continue
-                output.write(line + ' ' + 'normal' + '\n')
-                normal_count += 1
-        if crossing_count == positive_sample and normal_count == other_sample:
-            break
+            rd = random.randint(0, 99) + 1
+            if rd > fraction:
+                continue
+            output.write(line + ' ' + 'normal' + '\n')
+            normal_count += 1
     print("crossing count: ", crossing_count)
+    print("cormal count: ", normal_count)
 
 
-label_embeddings(f_nodes_selected, f_embeddings, f_labeled, positive_sample=1000, other_sample=1000 ,fraction=1)
+label_embeddings(f_nodes_selected, f_embeddings, f_labeled, fraction=1)
 
 f_labeled.close()
 f_embeddings.close()
