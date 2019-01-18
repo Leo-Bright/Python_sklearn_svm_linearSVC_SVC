6f10395c93d21af9536329c028780c7e9a86b5fc
Leo Bright <buxingzhexiong@sina.com>
2019-01-18 11:16:58 +0800

    初始化travel time regression代码

--- label_time_samples.py
+++ label_time_samples.py
@@ -16,17 +16,21 @@ def main(time_samples, embeddings, output):
     with open(time_samples, 'r') as time_samples_file:
         for line in time_samples_file:
             line = line.strip()
-            [start, end, travel_time] = line.split(' ')
-            start_emb = osmid_embeddings[start] if start in osmid_embeddings else None
-            end_emb = osmid_embeddings[end] if end in  osmid_embeddings else None
-            if not start_emb or not end_emb:
-                continue
-            result = [start, end] + start_emb + end_emb + [travel_time]
+            node_sequence_time = line.split(' ')
+            node_sequence = node_sequence_time[:-1]
+            travel_time = node_sequence_time[-1]
+            nodes_embeddings = []
+            for node in node_sequence:
+                if node not in osmid_embeddings:
+                    continue
+                nodes_embeddings.append(osmid_embeddings[node])
+
+            result = [node_sequence] + nodes_embeddings + [[travel_time]]   # how to compute result
             output_file.write('%s\n' % ' '.join(map(str, result)))
 
     output_file.close()
 
 
-main(time_samples='sanfrancisco/node/sf_travel_time_7.samples',
+main(time_samples='sanfrancisco/node/sf_travel_time_21.samples',
      embeddings='sanfrancisco/embedding/my_model/sanfrancisco_shortest_wn160_d128_ns5_ws5.embeddings',
-     output='sanfrancisco/labeled_emb/my_model/sanfrancisco_shortest_wn160_d128_ns5_ws5_time_7.embeddings',)
+     output='sanfrancisco/labeled_emb/my_model/sanfrancisco_shortest_wn160_d128_ns5_ws5_time_21.embeddings',)
--- travel_time_regression.py
+++ travel_time_regression.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
-import numpy as np
 import pandas as pd
-from sklearn import datasets, linear_model
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
-data_matrix = pd.read_csv('sanfrancisco/labeled_emb/my_model/sanfrancisco_shortest_wn160_d128_ns5_ws5_time_7.embeddings', header=None, sep=' ',)
-print(data_matrix.head())
-X = data_matrix[3:]
-print(X)
+
+def main(timed_embeddings):
+
+    reader = pd.read_csv(timed_embeddings, header=None, sep=' ', iterator=True)
+    df = reader.get_chunk(1000)
+    # print(df)
+    row_size, col_size = df.shape
+    data_features = df[list(range(2, col_size-1))]
+    data_label = df[col_size - 1]
+    train_features, test_features, train_label, test_label = train_test_split(data_features, data_label, random_state=1)
+
+    linear_regression = LinearRegression()
+    linear_regression.fit(train_features, train_label)
+    label_predict = linear_regression.predict(test_features)
+    print("MAE:", metrics.mean_absolute_error(test_label, label_predict))
+
+
+if __name__ == '__main__':
+    main(timed_embeddings='sanfrancisco/labeled_emb/my_model/sf_shortest_wn160_d128_ns5_ws5_time_21.embeddings',
+         )
